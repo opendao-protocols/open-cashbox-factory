ede2aa6ae80b44f99409df7fb4d07502cd603b56
akshaysrivastav <akshaysrivastav2014@gmail.com>
2020-10-30 14:41:01 +0530

    new contract addresses and abi

--- src/environments/blockchain-constants.ts
+++ src/environments/blockchain-constants.ts
@@ -1,6 +1,6 @@
 export const blockchainConstants = {
   mainnet: {
-    CashBoxFactory: '0xb8e2f589A5AffDFf6Ac94250A08EE555f74879CD',
+    CashBoxFactory: '0x83dd2B1D0bd6191EBb1b46512111D67df78490D5',
     cashTokens: {
       DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
       USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
@@ -11,7 +11,7 @@ export const blockchainConstants = {
     }
   },
   kovan: {
-    CashBoxFactory: '0x057a5B2506F17705067340142a2189d04e4E3AD5',
+    CashBoxFactory: '0x32F89beB709db30531560a2F970ACed582F7303d',
     cashTokens: {
       DAI: '0xaDA37B781132F9F8e6dF8784B1406eA91e6cB247',
       USDC: '0x46532dD5a52a3CB769990a525208d06ED12312D9',
